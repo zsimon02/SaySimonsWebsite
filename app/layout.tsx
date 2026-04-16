612b69bcafe8ae616f52335a5c3d4aeeec7c21ba
Zach Simon <zachsimon02@gmail.com>
2026-04-15 21:24:55 -0400

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { Cormorant_Garamond, Manrope } from "next/font/google";
 import "./globals.css";
 
@@ -54,7 +55,10 @@ export default function RootLayout({
       lang="en"
       className={`${manrope.variable} ${fraunces.variable} h-full scroll-smooth antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
